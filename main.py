c48e9c86a7164b1334aec568ff55683ee172f8ff
Ganeev Marwaha <161396831+ReptoBlazer@users.noreply.github.com>
2024-05-12 01:18:26 +0000

    Fixed background image for Homepage

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 
-#starting of program
+#Create Tkinter Object
 
 root = tk.Tk()
 
+#Read The Image
 image_path = ("Homepage.png")
-image_path = Image.open(image_path)
-image_path = image_path.resize((500, 500), Image.LANCZOS)
-image_path = ImageTk.PhotoImage(image_path)
+image = Image.open(image_path)
 
-label = tk.Label(root, image=image_path)
-label.pack()
+
+#Resize The Image
+width = 1920
+height = 1080
+resize_image = image.resize((width, height))
+
+img = ImageTk.PhotoImage(resize_image)
+
+#Add Image To Label
+label1 = tk.Label(root, image=img)
+label1.image = img
+label1.pack()
+
+#Execute Tkinter
 
 root.title("General Knowledge Quiz")
 root.mainloop()

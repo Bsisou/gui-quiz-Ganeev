0d0937d1c8d645636d1823233aec1612e2dbe003
Ganeev Marwaha <161396831+ReptoBlazer@users.noreply.github.com>
2024-05-13 02:44:11 +0000

    Added a button to my code

--- main.py
+++ main.py
@@ -1,9 +1,46 @@
 import tkinter as tk
+from tkinter import Frame, Label, Entry, Button
 from PIL import Image, ImageTk
 
-#Create Tkinter Object
+names_list = []
 
+class quizStarter:
+#Init fuction is used each time the class is being used to create a new object
+    def __init__(self, parent):
+      background_color = "slategray1"
+
+      
+      #Frame set up
+      self.quiz_frame = Frame(parent, bg = background_color, padx = 100, pady = 100)
+      self.quiz_frame.grid()
+
+      #Label widget for my heading
+      self.heading_label = Label(self.quiz_frame, text = "Welcome to the quiz", 
+ bg=background_color) 
+      self.heading_label.grid(row=0, pady=20)
+
+      #Label for username
+      self.user_label = Label (self.quiz_frame, text = "Please enter your name below", bg=background_color)
+      self.user_label.grid(row=1, pady=20)
+
+      #Label widget for my user name
+      self.entry_box=Entry(self.quiz_frame)
+      self.entry_box.grid(row=2, pady=20)
+
+      #Next button
+      self.next_button = Button(self.quiz_frame, text = "Next", bg = "blue", command=self.name_collection)
+      self.next_button.grid(row=3, pady=20)
+
+    def name_collection(self):
+        name = self.entry_box.get()
+        names_list.append(name)
+        print(names_list)
+        self.quiz_frame.destroy()
+    
+
+#Create Tkinter Object
 root = tk.Tk()
+
 
 #Read The Image
 image_path = ("Homepage.png")
@@ -11,9 +48,10 @@ image = Image.open(image_path)
 
 
 #Resize The Image
-width = 1920
-height = 1080
+width = 960
+height = 540
 resize_image = image.resize((width, height))
+
 
 img = ImageTk.PhotoImage(resize_image)
 
@@ -22,7 +60,13 @@ label1 = tk.Label(root, image=img)
 label1.image = img
 label1.pack()
 
-#Execute Tkinter
+next_button = tk.Button(root, text="Next")
+next_button.place(x=100, y=200)
 
-root.title("General Knowledge Quiz")
-root.mainloop()
+
+#Execute Tkinter
+if __name__ == "__main__":
+ root.title("General Knowledge Quiz")
+ #root = tk.Tk()
+ #quizStarter_object = quizStarter(root)
+ root.mainloop()

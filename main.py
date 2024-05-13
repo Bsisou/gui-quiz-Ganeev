31cc0fc8ce00de4e562578206c576f1d86ec8685
Ganeev Marwaha <161396831+ReptoBlazer@users.noreply.github.com>
2024-05-13 08:35:58 +0000

    Added a entry name widget, along with the button and centred them in the right position.

--- main.py
+++ main.py
@@ -5,68 +5,44 @@ from PIL import Image, ImageTk
 names_list = []
 
 class quizStarter:
-#Init fuction is used each time the class is being used to create a new object
     def __init__(self, parent):
       background_color = "slategray1"
 
-      
-      #Frame set up
-      self.quiz_frame = Frame(parent, bg = background_color, padx = 100, pady = 100)
-      self.quiz_frame.grid()
+      #Canvas setup to add the widgets on top of the Image like the button
+      self.canvas = tk.Canvas(parent, bg=background_color, width=960, height=540)
+      self.canvas.pack()
 
-      #Label widget for my heading
-      self.heading_label = Label(self.quiz_frame, text = "Welcome to the quiz", 
- bg=background_color) 
-      self.heading_label.grid(row=0, pady=20)
+      #Adding the image to the canvas
+      image_path = "Homepage.png"
+      self.image = Image.open(image_path)
+      self.resized_image = self.image.resize((960,540))
+      self.img = ImageTk.PhotoImage(self.resized_image)
+      self.canvas.create_image(0, 0, anchor='nw', 
+image=self.img)
 
-      #Label for username
-      self.user_label = Label (self.quiz_frame, text = "Please enter your name below", bg=background_color)
-      self.user_label.grid(row=1, pady=20)
+    
+      #Entry widget for my user name
+      self.entry_box = Entry(parent, bg="slategray1")
+      self.entry_box_window = self.canvas.create_window(480, 455, anchor='center', window=self.entry_box)
 
-      #Label widget for my user name
-      self.entry_box=Entry(self.quiz_frame)
-      self.entry_box.grid(row=2, pady=20)
-
-      #Next button
-      self.next_button = Button(self.quiz_frame, text = "Next", bg = "blue", command=self.name_collection)
-      self.next_button.grid(row=3, pady=20)
+      #Next button to proceed onto the next page
+      self.next_button = Button(parent, text = "Next", bg = "slategray1", command=self.name_collection)
+      self.next_button_window = self.canvas.create_window(750, 455, anchor='center', window=self.next_button)
 
     def name_collection(self):
         name = self.entry_box.get()
         names_list.append(name)
         print(names_list)
-        self.quiz_frame.destroy()
+        self.canvas.destroy()
     
 
 #Create Tkinter Object
 root = tk.Tk()
 
-
-#Read The Image
-image_path = ("Homepage.png")
-image = Image.open(image_path)
-
-
-#Resize The Image
-width = 960
-height = 540
-resize_image = image.resize((width, height))
-
-
-img = ImageTk.PhotoImage(resize_image)
-
-#Add Image To Label
-label1 = tk.Label(root, image=img)
-label1.image = img
-label1.pack()
-
-next_button = tk.Button(root, text="Next")
-next_button.place(x=100, y=200)
-
+# Create QuizStarter object
+quiz_starter_object = quizStarter(root)
 
 #Execute Tkinter
 if __name__ == "__main__":
  root.title("General Knowledge Quiz")
- #root = tk.Tk()
- #quizStarter_object = quizStarter(root)
  root.mainloop()
